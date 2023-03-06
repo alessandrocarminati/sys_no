de07a78559191f2e527745289b07860179267f4d
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-03-06 12:01:47 +0000

    clean up unused code
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- paths.c
+++ paths.c
@@ -1,23 +1,11 @@
 #include "cfg.h"
 #include "paths.h"
 
-/*
-static bool not_in2(uint64_t c, uint64_t visited[], int visited_no){
-        int i;
-	printf("not_in2, 0x%08lx\n", c);
-        for (i=0; i<visited_no; i++) {
-                if (visited[i]== c) return false;
-                }
-        return true;
-}
-*/
-
 
 int search_next(struct Block *current, int res_type, struct block_list *visited, struct block_list *path, int path_len, int *itn_no) {
 
 	visited->blocks[(visited->blocks_no)++]=current->start;
 	path->blocks[(path_len)++]=res_type==VIRTUAL_ADDRESS?current->start:current;
-//	*(itn_no)++;
 
 	if ((*(itn_no)<visited->blocks_no)&&(current->syscall)) {
 		path->blocks_no=path_len;
@@ -38,13 +26,3 @@ int search_next(struct Block *current, int res_type, struct block_list *visited,
 }
 
 
-/*
-int blockpath2syscall(struct Block *current, uint64_t *visited, uint64_t *path, int max_path) {
-
-	//invalidate any preexisting path
-	memset(path, 0, max_path*sizeof(uint64_t);
-	memset(visited, 0, max_path*sizeof(uint64_t);
-
-	gosearch(current, explored, path, max_path, 0
-}
-*/
