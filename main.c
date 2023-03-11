1196a1a32bf3286a41e3c4fdcb9cb3c7eb092bd7
Alessandro Carminati <alessandro.carminati@gmail.com>
2023-03-11 11:35:00 +0000

    Major cosmmethics
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "paths.h"
 #include "exec.h"
 #include "global_defines.h"
+#include "ansi_term.h"
 
 int execute_block_seq(struct exec_item *f, struct block_list *b){
 	uc_engine *uc;
@@ -43,33 +44,36 @@ int main(int argc, char *argv[]){
 	uc_engine *uc=NULL;
 
 	if (argc<=1) {
-		printf("wrong cmdline\n");
+		printf(BRED "[*]" RED " wrong cmdline\n"  reset);
 		return 1;
 		}
 	if ((index=strtol (argv[1],NULL,10))==0) {
-		printf("error\n");
+		printf(BRED "[*]" RED " error\n" reset);
 		return 1;
 		}
 	if (index>sizeof(f)+1) {
-		printf("index out of range\n");
+		printf(BRED "[*]" RED " index out of range\n" reset);
 		return 1;
 		}
-	printf("function name %s, starting analysis\n", f[index]->name);
+	printf(BGRN "[*]" GRN " Function name "HYEL"%s"GRN", starting analysis\n" reset, f[index]->name);
+	printf(BGRN "[*]" GRN " Block statistics:\n" reset);
 	v.blocks=(uint64_t *) malloc(MAX_BLOCKS*sizeof(uint64_t));
 	p.blocks_addr=(struct Block **) malloc(MAX_BLOCKS*sizeof(uint64_t));
 
 	init_res();
 	root=build_cfg(f[index]);
 	print_plain_cfg(root);
+	printf(BGRN "[*]" GRN " Generating cfg for the given function\n" reset);
 	printf("%s", cfg2dot(root));
+	printf(BGRN "[*]" GRN " Generating paths and evaluating syscall number through guided execution\n" reset);
 	while (search_next(root, HOST_ADDRESS, &v, &p, 0, &tmp)!=NO_FOUND) {
-		DBG_PRINT("Path found!\n");
+		DBG_PRINT(BRED "[*]" RED " Path found!\n");
 		for (i=0; i<p.blocks_no; i++) {
 			printf("0x%08x, ", p.blocks_addr[i]->start);
 			}
 		printf("\n");
 		if (execute_block_seq(f[index], &p)) {
-			printf("Premature termination!!!\n");
+			printf(BRED "[*]" RED " Premature termination!!!\n" reset);
 			break;
 			}
 		}
